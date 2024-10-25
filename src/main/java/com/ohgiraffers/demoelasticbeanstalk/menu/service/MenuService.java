41c6710d7f5f35f50e08b9a9a6149f55d829cbed
Jinho <tiger.ohgiraffers@gmail.com>
2024-10-25 14:21:38 +0900

    feat: github action test1

--- src/main/java/com/ohgiraffers/demoelasticbeanstalk/menu/service/MenuService.java
+++ src/main/java/com/ohgiraffers/demoelasticbeanstalk/menu/service/MenuService.java
@@ -1,4 +1,29 @@
 package com.ohgiraffers.demoelasticbeanstalk.menu.service;
 
+import com.ohgiraffers.demoelasticbeanstalk.menu.dto.MenuDto;
+import com.ohgiraffers.demoelasticbeanstalk.menu.entity.Menu;
+import com.ohgiraffers.demoelasticbeanstalk.menu.repository.MenuRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
 public class MenuService {
+
+    private final MenuRepository menuRepository;
+
+    public List<MenuDto> getAllMenus() {
+
+        List<Menu> menus = menuRepository.findAll();
+
+        List<MenuDto> menuDtos = menus.stream().map(MenuDto::new).collect(Collectors.toList());
+
+        return menuDtos;
+    }
 }
